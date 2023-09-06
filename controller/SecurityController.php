bc9ee7bb343c991b349196609c9586e92d893a6f
euroR67 <mchamaev67@gmail.com>
2023-09-06 16:27:37 +0200

    système d'inscription finalisée

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -15,20 +15,54 @@
             // On instancie le manager
             $userManager = new UserManager();
 
-            // On filtre les champs de saisis
-            $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $password1 = filter_input(INPUT_POST, 'password1', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $password2 = filter_input(INPUT_POST, 'password2', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            
-            // On vérifie que le filtrage c'est bien passée
-            if($email && $pseudo && $password1 && $password2 
-            && !$userManager->findOneByEmail($email) 
-            && !$userManager->findOneByPseudo($pseudo)) {
-                var_dump("ok");die;
+            if(isset($_POST["submit"])) {
+
+                // On filtre les champs de saisis
+                $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $password1 = filter_input(INPUT_POST, 'password1', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $password2 = filter_input(INPUT_POST, 'password2', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
                 
-            } else {
-                // Message ou Action au cas ou le filtrage ne passe pas
+                // On vérifie que le filtrage c'est bien passée
+                if($email && $pseudo && $password1 && $password2) {
+                    // On vérifie si l'email ou pseudo existe ou non
+                    if(!$userManager->findOneByEmail($email) && !$userManager->findOneByPseudo($pseudo)) {
+                        // On vérifie que les 2 password correspondent et que le mdp contient au moins 8 caractères
+                        if($password1 === $password2 ) {
+                            // On vérifie que le mot de passe comporte plus de 8 caractères
+                            if(strlen($password1) >= 8) {
+
+                                // On hache le mot de passe avec BCRYPT (par défaut)
+                                $password = password_hash($password1, PASSWORD_DEFAULT);
+                                // On récupère les données du formulaire
+                                $data = [
+                                    'pseudo' => $pseudo ,
+                                    'password' => $password,
+                                    'role' => 'ROLE_USER',
+                                    'email' => $email
+                                ];
+                                // On ajoute l'utilisateur a la base de donnée
+                                $userManager -> add($data);
+
+                                // Si inscription réussi on redirige vers la page login
+                                $this->redirectTo("security", "login");
+
+                            } else {
+                                // Message ou action au cas ou le mot de passe est trop court
+                                Session::addFlash("error", "Le mot de passe est trop court");
+                            }
+                        } else {
+                            // Message ou action au cas ou le mot de passe n'est pas identique
+                            Session::addFlash("error", "Les mots de passe ne sont pas identique");
+                        }
+                    } else {
+                        // Message ou Action au cas ou l'utilisateur existe déjà
+                        Session::addFlash("error", "Un utilisateur existe déjà avec cet email ou pseudo");
+                    }
+                } else {
+                    // Message ou Action au cas ou le filtrage ne passe pas
+                    Session::addFlash("error", "Vérifier vos saisis...");
+                }
             }
 
             // On redirige vers la page d'inscription
@@ -43,6 +77,13 @@
 
             // On instancie le manager
             $userManager = new UserManager();
+
+            if(isset($_POST["submit"])) {
+                
+                // On filtre les champs de saisis
+                $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $password1 = filter_input(INPUT_POST, 'password1', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            }
 
             // On redirige vers la page de login
             return [
--- view/security/register.php
+++ view/security/register.php
@@ -4,21 +4,21 @@
 <form action="index.php?ctrl=security&action=register" method="post">
     <div>
         <label for="email">Email :</label>
-        <input type="email" name="email" id="email" required>
+        <input type="email" name="email" id="email" >
     </div>
     <div>
         <label for="pseudo">Pseudo :</label>
-        <input type="text" name="pseudo" id="pseudo" required>
+        <input type="text" name="pseudo" id="pseudo" >
     </div>
     <div>
         <label for="password">Mot de passe :</label>
-        <input type="password" name="password1" id="password1" required>
+        <input type="password" name="password1" id="password1"  >
     </div>
     <div>
         <label for="password">Confirmation du mot de passe :</label>
-        <input type="password" name="password2" id="password2" required>
+        <input type="password" name="password2" id="password2"  >
     </div>
     <div>
-        <input type="submit" value="S'enregistrer">
+        <input type="submit" name="submit" id="submit" value="S'enregistrer">
     </div>
 </form>
