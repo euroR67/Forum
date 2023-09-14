8d1c6045d91801e8e1044ed16e9ffa441743a8d0
euroR67 <mchamaev67@gmail.com>
2023-09-14 10:11:43 +0200

    amélioration du front homepage mobile version

--- view/home.php
+++ view/home.php
@@ -1,13 +1,45 @@
-<div>
-    <h1>CDA</h1>
-</div>
+<div class="main-home">
+  <div class="logo">
+      <h1>CDA</h1>
+  </div>
 
-<div>
-    <h2>Bienvenue sur "CDA" FORUM</h2>
-    <p>Le tout nouveau forum du net!</p>
+  <div class="welcome">
+      <h2>Bienvenue</h2>
+      <h2>sur "CDA" FORUM</h2>
+      <p>Le tout nouveau forum du net!</p>
 
-    <div class="search-container">
-      <input type="text" placeholder="Entrée un mot clé.." name="search">
-      <button type="submit"><i class="fa fa-search"></i></button>
-    </div>
+      <div class="search-container">
+        <input type="text" placeholder="Entrée un mot clé.." name="search">
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </div>
+  </div>
+  
+  <div class="join-us">
+    <h3>REJOIGNEZ NOTRE FORUM !</h3>
+    <p>Parlez de tout ce qui vous passe par la tête et voyez ce que pensent les autres. En tant qu'invité de notre forum, vous ne pouvez consulter que les messages. Lorsque vous vous inscrivez sur le forum Forumix, vous pouvez participer à des sujets, créer de nouveaux sujets et généralement faire partie du premier niveau de notre communauté.</p>
+    <a href="index.php?ctrl=security&action=register">S'INSCRIRE !</a>
+  </div>
+
+  <div class="forums">
+      <h3>FORUMS</h3>
+      <ul>
+          <li>
+              <a href="">Annonces</a>
+              <span class="closed">Fermer</span>
+              <p>Ce forum est spécialement pour des annonces générals</p>
+          </li>
+          <li>
+              <a href="">Technologie</a>
+              <p>Tout ce qui concerne la technologie</p>
+          </li>
+          <li>
+              <a href="">High-tech</a>
+              <p>Tout sur le high-tech</p>
+          </li>
+          <li>
+              <a href="">Informatique</a>
+              <p>Hardware, software, développement, etc.</p>
+          </li>
+      </ul>
+  </div>
 </div>
